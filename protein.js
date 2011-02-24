72e96b1a1e8eb8fd4b878e56d4d0d69425a6558e
Brian Doll <brian@emphaticsolutions.com>
2011-02-24 15:26:26 -0800

    UI upgradez

--- protein.js
+++ protein.js
@@ -16,6 +16,23 @@ $(function() {
   });
 });
 
+var progressMessage = function(percentage) {
+  var msg = "Let's get some chow!";
+  if (percentage < 25) {
+    msg = "Keep eatin'";
+  } else if (percentage < 50) {
+    msg = "Your on your way!";
+  } else if (percentage < 75) {
+    msg = "Getting closer!";
+  } else if (percentage < 100) {
+    msg = "How about some protein snacks!";
+  } else if (percentage > 99) {
+    msg = "Congrats! This is enough protein for you on a given day.";
+  }
+  return msg;
+}
+
+
 var updateProgressBar = function() {
   var total_protein_grams = Number(0);
   var qtys = $("#food ul").children().find('.qty');
@@ -27,12 +44,10 @@ var updateProgressBar = function() {
   var goal = $("#protein_goal").val();
   var percentage = (total_protein_grams / Number(goal) * 100);
   $("#progressbar").progressbar({ value: percentage });
-  if (percentage > 99) {
-    $("#congrats").show();
-  }
+  $("#percent_complete").html(percentage + "%");
+  $("#congrats").html(progressMessage(percentage));
 };
 
-$("#congrats").hide();
 $("#progressbar").progressbar({ value: 0 });
 $("#food ul li .qty").live('change', updateProgressBar);
 $("#food ul li .protein_source").live('change', updateProgressBar);
